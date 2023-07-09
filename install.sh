9e3fcad95f004ca4e3bb69dd2b8545fe83acd9a5
Shubham Dubey <shubham@Shubhams-MBP.domain.name>
2023-07-10 00:14:08 +0530

    first commit

---
+++ install.sh
@@ -0,0 +1,31 @@
+echo "bash folder started"
+
+echo "client folder started"
+cd client
+yarn install
+cd ../
+
+echo "comments folder started"
+cd comments
+yarn install
+cd ../
+
+echo "event-bus folder started"
+cd event-bus
+yarn install
+cd ../
+
+echo "moderation folder started"
+cd moderation
+yarn install
+cd ../
+
+echo "posts folder started"
+cd posts
+yarn install
+cd ../
+
+echo "posts folder started"
+cd posts
+yarn install
+cd ../
